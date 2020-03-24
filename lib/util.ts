1abcbe205f08fc52093433ad0d32f147f81812c6
Kai Raschke <kr@kaju.it>
2020-03-24 16:42:53 +0100

    feat: authority can set status for user, gets anonymous alias

--- lib/util.ts
+++ lib/util.ts
@@ -15,6 +15,24 @@ function randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low)
 }
 
+const colorArray = [
+    'schwarz', 'weiß', 'grün', 'gelb', 'rot', 'blau', 'lila'
+];
+const alphabetArray = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' , 'W', 'X', 'Y', 'Z'
+];
+function createAlias() {
+    // Create some randomness
+    const randomColor1 = colorArray[Math.floor(Math.random() * colorArray.length)];
+    const randomColor2 = colorArray[Math.floor(Math.random() * colorArray.length)];
+    const randomAlphabet = alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
+    const randomInt1 = leftPad(randomInt(0, 99), 2);
+    const randomInt2 = leftPad(randomInt(0, 99), 2);
 
+    // Form readable string
+    const alias = `${randomAlphabet} ${randomInt1} ${randomColor1} ${randomColor2} ${randomInt2}`;
 
-export { leftPad, randomInt };
+    return alias;
+}
+
+export { leftPad, randomInt, createAlias };
--- routes/AuthorityRouter.js
+++ routes/AuthorityRouter.js
@@ -10,10 +10,54 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Router = require("koa-router");
+const db_1 = require("../lib/db");
+const log_1 = require("../lib/log");
+const util_1 = require("../lib/util");
 exports.authorityRouter = new Router()
     .get('/secret', function (ctx) {
     return __awaiter(this, void 0, void 0, function* () {
         ctx.body = "secret";
     });
-});
+})
+    .post('/authState', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    let { uid, status } = ctx.request.body;
+    let aid = ctx.state.user.dbid;
+    let exists = null;
+    let alias, loops = 0;
+    do {
+        alias = util_1.createAlias();
+        loops++;
+        exists = yield db_1.Data.Entry.findOne({
+            where: {
+                alias
+            }
+        });
+    } while (exists && loops < 99999);
+    if (loops === 99999) {
+        ctx.status = 404;
+        ctx.body = "No free alias. Try again later";
+    }
+    else {
+        try {
+            let entry = yield db_1.Data.Entry.findOne({
+                attributes: ['id', 'uid', 'status', 'key'],
+                where: {
+                    uid
+                }
+            });
+            let authority = yield db_1.Data.Authority.findOne({ aid });
+            entry.alias = alias;
+            entry.status = status;
+            log_1.log.info('status authority', { status });
+            yield entry.save();
+            entry.setAuthority(authority);
+            ctx.status = 200;
+            ctx.body = { alias };
+        }
+        catch (ex) {
+            console.error(ex);
+            ctx.status = 500;
+        }
+    }
+}));
 //# sourceMappingURL=AuthorityRouter.js.map
